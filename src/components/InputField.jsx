1908af71679135269be08e4a79e2964a44196144
Niteshzzzzz <niteshkumarnkzzzzz@gmail.com>
2025-03-23 19:09:17 +0530

    updated

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function InputField({label, type}) {
     return (
         <div className="relative mb-6">
-            <input type={type} id="name" name='name' placeholder='Marry Doe' className='border-1 border-[#CBCBCB] rounded text-black-700 px-4 py-2 w-full focus:outline-[#6C25FF]' ></input>
+            <input type={type} id="name" name={label} placeholder='Marry Doe' className='border-1 border-[#CBCBCB] rounded text-black-700 px-4 py-2 w-full focus:outline-[#6C25FF]' ></input>
             <label htmlFor="name" className='absolute top-[-0.8rem] left-[1.2rem] bg-[#F7F8F9] text-[#6C25FF] px-[4px] text-sm'>{label} <sup className='text-[#DD4A3D]'>*</sup></label>
         </div>
     )
