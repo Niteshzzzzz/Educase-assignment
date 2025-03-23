23d72fa16f154111d9941a1de9298f3798884cd2
Niteshzzzzz <niteshkumarnkzzzzz@gmail.com>
2025-03-23 19:00:24 +0530

    updated

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import InputField from './InputField'
 
 function CreateAccount() {
@@ -13,15 +12,15 @@ function CreateAccount() {
 
     return (
         <div className="w-[375px] min-h-[612px] border-2 rounded border-[#F7F8F9] flex flex-col bg-[#F7F8F9]">
-            <form action="" className='h-[612px] px-4 py-6 flex flex-col justify-between'>
+            <form action="/account" className='h-[612px] px-4 py-6 flex flex-col justify-between'>
                 <div className="">
                     <h2 className='text-[28px] font-bold mb-6'>Create your PopX account</h2>
                     <div className="">
-                        <InputField label={'Full Name'} />
-                        <InputField label={'Phone number'} />
-                        <InputField label={'Email address'} />
-                        <InputField label={'Password'} />
-                        <InputField label={'Company name'} />
+                        <InputField label={'Full Name'} type={'text'} />
+                        <InputField label={'Phone number'} type={'text'} />
+                        <InputField label={'Email address'} type={'email'} />
+                        <InputField label={'Password'} type={'password'} />
+                        <InputField label={'Company name'} type={'text'} />
                         <div className="">
                             <h3 className='text-sm mb-1 mt-[-10px]'>Are you an Agency? <sup className='text-[#DD4A3D]'>*</sup></h3>
                             <label className='mr-4 text-sm'>
@@ -51,7 +50,7 @@ function CreateAccount() {
                     </div>
                 </div>
                 <div className="">
-                    <Link to={'/account'} className='bg-[#6C25FF] mt-8 text-white flex justify-center items-center p-2 text-base rounded'>Create Account</Link>
+                    <button to={'/account'} className='w-full bg-[#6C25FF] mt-8 text-white flex justify-center items-center p-2 text-base rounded'>Create Account</button>
                 </div>
             </form>
         </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import InputField from './InputField'
 
 function Login() {
   return (
     <div className="max-w-[375px] h-[612px] border-2 rounded border-[rgb(247,248,249)] flex flex-col px-4 py-6 bg-[#F7F8F9]">
-      <h2 className='text-[28px] font-bold'>Welcome to PopX</h2>
-      <p className='text-lg opacity-60 mb-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-      <InputField label={'Email address'} />
-      <InputField label={'Password'} />
-      <Link to={'/account'} className='bg-[#CBCBCB] mt-1 text-white flex justify-center items-center p-2 text-base rounded'>Login</Link>
+      <form action="/account">
+        <h2 className='text-[28px] font-bold'>Welcome to PopX</h2>
+        <p className='text-lg opacity-60 mb-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        <InputField label={'Email address'} />
+        <InputField label={'Password'} />
+        <button className='w-full bg-[#CBCBCB] mt-1 text-white flex justify-center items-center p-2 text-base rounded'>Login</button>
+      </form>
     </div>
   )
 }
